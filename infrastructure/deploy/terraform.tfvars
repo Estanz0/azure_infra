f882d918f7bfda9206ba871d0f936f879427efb4
Byron Smith <68315031+Estanz0@users.noreply.github.com>
2024-02-25 19:59:28 +1300

    update

--- infrastructure/deploy/terraform.tfvars
+++ infrastructure/deploy/terraform.tfvars
@@ -4,7 +4,3 @@ location   = "australiaeast"
 
 # Service Plan
 asp_os_type = "Linux"
-
-# Github
-gh_repo_owner = "Estanz0"
-gh_repo_name  = "azure_infra"
--- infrastructure/plan.local.sh
+++ infrastructure/plan.local.sh
@@ -15,4 +15,4 @@ az account set -s $ARM_SUBSCRIPTION_ID
 terraform -chdir=deploy init 
 terraform -chdir=deploy fmt 
 terraform -chdir=deploy workspace select -or-create=true $env$project
-terraform -chdir=deploy plan  -input=false -var-file $env.tfvars -var env=$env -var project_id=$project -var client_id=$ARM_CLIENT_ID -var client_secret=$ARM_CLIENT_SECRET -var subscription_id=$ARM_SUBSCRIPTION_ID -var tenant_id=$ARM_TENANT_ID
+terraform -chdir=deploy plan  -input=false -var-file $env.tfvars -var env=$env -var project_id=$project -var gh_repo_owner=$GH_REPO_OWNER -var gh_repo_name=$GH_REPO_NAME -var client_id=$ARM_CLIENT_ID -var client_secret=$ARM_CLIENT_SECRET -var subscription_id=$ARM_SUBSCRIPTION_ID -var tenant_id=$ARM_TENANT_ID
